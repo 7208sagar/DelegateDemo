dc45fd4fc79b29a5ec79d6b7d4ebe3b87804745c
sagar <7208sk@gmail.com>
2021-10-28 23:13:09 +0530

    delegate practice demo

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace DelegateDemo
+{
+    class Program
+
+    {
+        public delegate void MyDelegates(int a, int b);
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            MyDelegates del = new MyDelegates(Add);
+            del(2, 4);
+
+        }
+        public static void Add(int a, int b)
+        {
+            Console.WriteLine(a + b);
+        }
+    }
+}
+
